590cf6b5dfa60001177c3e402980a81b993c2e05
houbb <1060732496@qq.com>
2018-06-09 17:59:05 +0800

    [Feature] add for dynamic var and collection

---
+++ qlexpress/src/test/java/com/github/houbb/tech/validation/qlexpress/CollectionTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c)  2018. houbinbin Inc.
+ * tech-validation All rights reserved.
+ */
+
+package com.github.houbb.tech.validation.qlexpress;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+
+import org.junit.Test;
+
+/**
+ * <p> 集合 </p>
+ *
+ * @author houbinbin
+ * @version 1.0
+ * @since JDK 1.7
+ */
+public class CollectionTest {
+
+    /**
+     * set 集合测试
+     * 备注：集合的快捷写法
+     * @throws Exception if any
+     */
+    @Test
+    public void setTest() throws Exception {
+        ExpressRunner runner = new ExpressRunner(false,false);
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        String express = "abc = NewMap(1:1,2:2); return abc.get(1) + abc.get(2);";
+        Object r = runner.execute(express, context, null, false, false);
+        System.out.println(r);
+        express = "abc = NewList(1,2,3); return abc.get(1)+abc.get(2)";
+        r = runner.execute(express, context, null, false, false);
+        System.out.println(r);
+        express = "abc = [1,2,3]; return abc[1]+abc[2];";
+        r = runner.execute(express, context, null, false, false);
+        System.out.println(r);
+    }
+
+    /**
+     * 遍历测试
+     * 其实类似java的语法，只是ql不支持for(obj:list){}的语法，只能通过下标访问。
+     */
+    @Test
+    public void foreachTest() throws Exception {
+        ExpressRunner runner = new ExpressRunner(false,false);
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        String express =
+                "  Map map = new HashMap();\n" +
+                "  map.put(\"a\", \"a_value\");\n" +
+                "  map.put(\"b\", \"b_value\");\n" +
+                "  keySet = map.keySet();\n" +
+                "  objArr = keySet.toArray();\n" +
+                "  for (i=0;i<objArr.length;i++) {\n" +
+                "  key = objArr[i];\n" +
+                "   System.out.println(map.get(key));\n" +
+                "  }";
+        Object r = runner.execute(express, context, null, false, false);
+        System.out.println(r);
+    }
+
+}
---
+++ qlexpress/src/test/java/com/github/houbb/tech/validation/qlexpress/DynamicVarTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c)  2018. houbinbin Inc.
+ * tech-validation All rights reserved.
+ */
+
+package com.github.houbb.tech.validation.qlexpress;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.DynamicParamsUtil;
+import com.ql.util.express.ExpressRunner;
+import com.ql.util.express.IExpressContext;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * <p> 可变参数 </p>
+ *
+ * @author houbinbin
+ * @version 1.0
+ * @since JDK 1.7
+ */
+public class DynamicVarTest {
+
+    /**
+     * 获取模板
+     * 备注: 等价于getTemplate(Object[] params)
+     * @param params 参数
+     * @return 结果
+     * @throws Exception if any
+     */
+    public Object getTemplate(Object... params) throws Exception{
+        StringBuilder result = new StringBuilder();
+        for(Object obj:params){
+            result.append(obj).append(",");
+        }
+        return result.toString();
+    }
+
+    /**
+     * 动态参数测试
+     * @throws Exception if any
+     */
+    @Test
+    public void dynamicVarTest() throws Exception {
+        ExpressRunner runner = new ExpressRunner();
+        IExpressContext<String,Object> expressContext = new DefaultContext<>();
+        runner.addFunctionOfServiceMethod("getTemplate", this, "getTemplate", new Class[]{Object[].class}, null);
+
+        //(1)默认的不定参数可以使用数组来代替
+        Object r = runner.execute("getTemplate([11,'22',33L,true])", expressContext, null,false, false);
+        System.out.println(r);
+        //(2)像java一样,支持函数动态参数调用,需要打开以下全局开关,否则以下调用会失败
+        DynamicParamsUtil.supportDynamicParams = true;
+        r = runner.execute("getTemplate(11,'22',33L,true)", expressContext, null,false, false);
+        System.out.println(r);
+    }
+
+}
